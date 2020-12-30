6d7f0f385ecfe5501dbc80505df25ac17497d80d
ajayoneness <42382145+ajayoneness@users.noreply.github.com>
2020-12-30 22:56:20 +0545

    Add files via upload

---
+++ dictonary.py
@@ -0,0 +1,10 @@
+diclist={
+     "box" : "daba",
+     "thing":"bastu",
+     "love" : "pyar",
+     "breakup":"dil tut gaya"
+
+     }
+print ("\n",diclist.keys())
+p= input('enter the name of the word :')
+print("\n\n\tthe meaning of ",p ,"is :",diclist.get(p))
